e30dd1408e5cad3c296f35c30d3d837734144ae2
Sherilyn Moline <smoline@gmail.com>
2017-04-09 15:45:44 -0400

    Epic Mode Final
    Stories index updated with comment count and comments no longer repeating on stories show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to story_path(@story)
   end
 
+  def destroy
+      @story = Story.find(params[:story_id])
+      @comment = @story.comments.find(params[:id])
+      @comment.destroy
+      redirect_to story_path(@story)
+    end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
